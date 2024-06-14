1fd6c6d7ea777af208330a7aa38a2591f7a76679
Luan Otoni <luanotoni4@gmail.com>
2024-06-14 09:11:25 -0300

    Commit Passo 12 incompleto.

--- app/src/main/java/otoni/luan/galeria/MainActivity.java
+++ app/src/main/java/otoni/luan/galeria/MainActivity.java
@@ -1,9 +1,14 @@
 package otoni.luan.galeria;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -22,6 +27,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -41,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
     String currentPhotoPath;
     List<String> photos = new ArrayList<>();
 
+    static int RESULT_REQUEST_PERMISSION = 2;
 
     MainAdapter mainAdapter;
     @Override
@@ -66,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
             GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, numberOfColumns);
             rvGallery.setLayoutManager(gridLayoutManager);
         }
+
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.CAMERA);
+        checkForPermissions(permissions);
+
     }
 
 
@@ -134,4 +146,50 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra("photo_path",photoPath);
         startActivity(i);
     }
+
+    private void checkForPermission(List<String> permissions) {
+        List<String> permissionNotGranted = new ArrayList<>();
+        for (String permission : permission : permissions){
+            if(!hasPermission(permission)) {
+                permisionsNotGranted.add(permission);
+            }
+        }
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+            if (permissionNotGranted.size()>0){
+                requestPermissions(permissionNotGranted.toArray(new String[permissionsNotGranted.size()]),RESULT_REQUEST_PERMISSION);
+            }
+        }
+    }
+
+    private boolean hasPermission(String permission){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        final List<String> permissionsRejected = new ArrayList<>();
+        if (requestCode == RESULT_REQUEST_PERMISSION){;
+            for(String permission : permissions){
+                if (!hasPermission(permission)){
+                    permissionsRejected.add(permission);
+                }
+            }
+        }
+
+        if(permissionsRejected.size() > 0){
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))){
+                    new AlertDialog.Builder(MainActivity.this).setMessage("Para usar essa app é preciso conceder essas permissões").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which){
+                            requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                        }
+                    }).create().show();
+                }
+            }
+        }
 }
