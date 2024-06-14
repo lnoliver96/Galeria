ea92dae9f1e77b863fd0d84321e9583fb21fa3b5
Luan Otoni <luanotoni4@gmail.com>
2024-06-14 08:07:38 -0300

    Commit Passo 11 finalizado.

--- app/src/main/java/otoni/luan/galeria/MainActivity.java
+++ app/src/main/java/otoni/luan/galeria/MainActivity.java
@@ -1,5 +1,6 @@
 package otoni.luan.galeria;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -18,6 +19,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.FileProvider;
@@ -29,7 +31,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -102,7 +106,27 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private File createImageFile() {
+    private File createImageFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File f = File.createTempFile(imageFileName, ".jpg",storageDir);
+        return f;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == RESULT_TAKE_PICTURE){
+            if (requestCode == Activity.RESULT_OK){
+                photos.add(currentPhotoPath);
+                mainAdapter.notifyItemInserted(photos.size()-1);
+            }
+            else {
+                File f = new File(currentPhotoPath);
+                f.delete();
+            }
+        }
     }
 
     public void startPhotoActivity(String photoPath) {
